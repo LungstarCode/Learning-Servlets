275ca0199f7fadcc26137715f275ab25cc3a0494
LungstarCode <lungilemawila5@gmail.com>
2026-07-26 20:33:20 +0200

    created a first server web app with 1 html that callects a name and city and pass it to a Servlet that then harvest the values using a doPost method, then print a statment with the 2 values using a PrintWriter

---
+++ src/main/java/FirstServletClass.java
@@ -0,0 +1,37 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServletClass
+ */
+@WebServlet("/firstServlet")
+
+public class FirstServletClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    
+    public FirstServletClass() {
+        
+    }
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		String city = request.getParameter("city");
+		
+		PrintWriter writer = response.getWriter();
+		
+		writer.print("Hello "+ name + "I am happy I met someone from "+ city);
+		
+	}
+
+}
